7a38cd75e079d26f79d28cbaa50cb54a71f5bb6b
crystalittle <104913896+crystalittle@users.noreply.github.com>
2024-07-19 11:58:09 -0300

    19-07

---
+++ resources/views/base.blade copy.php
@@ -0,0 +1,25 @@
+{{--resources/views/animais/base.blade.php--}}
+<html>
+    <head>
+        <title>@yield('titulo')</title>
+    </head>
+    <body>
+        <h1>@yield('titulo')</h1>
+        <hr>
+        <a href="{{ route('index') }}">Inicial</a>
+        |
+        <a href="{{ route('animais') }}">Animais</a>
+        | 
+        @if (Auth::user() && Auth::user()['admin'])
+         <a href="{{ route('usuarios') }}">Usuários</a>
+         @endif
+        @if(Auth::user())
+        Olá <strong>{{ Auth::user()['nome'] }}</strong>
+        <a href="{{ route('logout') }}">Logout</a>
+        @else
+        <a href="{{ route('login') }}">Login</a>
+        @endif
+        <hr>
+        @yield('conteudo')
+    </body>
+</html>
